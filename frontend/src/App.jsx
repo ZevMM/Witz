062c2d3a95924acc02df06a4499eda811fbc1722
ZevMM <zevmcmanusmendelowitz@gmail.com>
2024-08-14 16:36:26 -0500

    Removed commodities and hedge funds

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -59,12 +59,10 @@ function App() {
   const simprops = {mall: mall, setMall: setMall, view: view, setView: setView, mlayout: mlayout, setMlayout: setMlayout, mevents: mevents, setMevents: setMevents, simData: simData, setSimData: setSimData, range: range, setRange: setRange}
   const [levs, setLevs] = useState([])
   const [portfolio, setPortfolio] = useState([
-    { id: 1, alt: "1", title: "Bonds", data: [["Type", "Principal", "Date", "Leverage"]], open: true},
-    { id: 2, alt: "commodities", title: "Commodities", data: [["Term", "Principal", "Date", "Leverage"]], open: true},
+    { id: 1, alt: "bonds", title: "Bonds", data: [["Type", "Principal", "Date", "Leverage"]], open: true},
     { id: 3, alt:"crypto", title: "Cryptocurrency", data: [["Name", "Principal", "Date", "Leverage"]], open: true},
     { id: 4, alt:"currency", title: "Currency", data: [["Symbol", "Principal", "Date", "Leverage"]], open: true},
     { id: 5, alt:"ETFs", title: "Exchange Traded Funds", data: [["Symbol", "Principal", "Date", "Leverage"]], open: true},
-    { id: 6, alt:"6", title: "Hedge Funds", data: [["Name", "Principal", "Date", "Leverage"]], open: true},
     { id: 7, alt:"mutualFunds", title: "Mutual Funds", data: [["Name", "Principal", "Date", "Leverage"]], open: true},
     { id: 8, alt:"realEstate", title: "Real Estate", data: [["Region", "Principal", "Date", "Leverage"]], open: true},
     { id: 9, alt:"monthlyStock", title: "Stocks", data: [["Symbol", "Principal", "Date", "Leverage"]], open: true},
